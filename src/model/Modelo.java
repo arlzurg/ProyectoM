87bb3b92c8633831586728b38ee7994b4e686a1f
arlzurg <alejandro.sonnor@gmail.com>
2016-06-14 01:19:19 -0500

    Update

---
+++ src/model/Modelo.java
@@ -0,0 +1,37 @@
+package model;
+
+import java.util.List;
+
+public class Modelo implements ICrud<Persona> {
+
+	@Override
+	public void create(Persona t) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(Persona t) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(Persona t) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Persona find(int t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Persona> ReadAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/model/Persona.java
+++ src/model/Persona.java
@@ -10,39 +10,64 @@ public class Persona{
 	private int edad;
 	private Date fecNac;
 	
+	
+	public Persona() {
+		
+	}
+
+	public Persona(String dni, String nombres, String apellidos, String sexo, int edad, Date fecNac) {
+		this.dni = dni;
+		this.nombres = nombres;
+		this.apellidos = apellidos;
+		this.sexo = sexo;
+		this.edad = edad;
+		this.fecNac = fecNac;
+	}
+	
 	public String getDni() {
 		return dni;
 	}
+	
 	public void setDni(String dni) {
 		this.dni = dni;
 	}
+	
 	public String getNombres() {
 		return nombres;
 	}
+	
 	public void setNombres(String nombres) {
 		this.nombres = nombres;
 	}
+	
 	public String getApellidos() {
 		return apellidos;
 	}
+	
 	public void setApellidos(String apellidos) {
 		this.apellidos = apellidos;
 	}
+	
 	public String getSexo() {
 		return sexo;
 	}
+	
 	public void setSexo(String sexo) {
 		this.sexo = sexo;
 	}
+	
 	public int getEdad() {
 		return edad;
 	}
+	
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
+	
 	public Date getFecNac() {
 		return fecNac;
 	}
+	
 	public void setFecNac(Date fecNac) {
 		this.fecNac = fecNac;
 	}
