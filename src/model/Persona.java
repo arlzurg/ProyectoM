59caf05c60ffeb6d71eaa8ed60241976b767f03f
arlzurg <alejandro.sonnor@gmail.com>
2016-06-14 03:35:37 -0500

    quilombo

--- src/model/Persona.java
+++ src/model/Persona.java
@@ -9,7 +9,7 @@ public class Persona{
 	private String sexo;
 	private int edad;
 	private Date fecNac;
-	
+	private char v;
 	
 	public Persona() {
 		
@@ -22,54 +22,43 @@ public class Persona{
 		this.sexo = sexo;
 		this.edad = edad;
 		this.fecNac = fecNac;
+		v = ' ';
 	}
 	
 	public String getDni() {
 		return dni;
 	}
 	
-	public void setDni(String dni) {
-		this.dni = dni;
-	}
-	
 	public String getNombres() {
 		return nombres;
 	}
 	
-	public void setNombres(String nombres) {
-		this.nombres = nombres;
-	}
-	
+		
 	public String getApellidos() {
 		return apellidos;
 	}
 	
-	public void setApellidos(String apellidos) {
-		this.apellidos = apellidos;
-	}
-	
+		
 	public String getSexo() {
 		return sexo;
 	}
 	
-	public void setSexo(String sexo) {
-		this.sexo = sexo;
-	}
-	
+		
 	public int getEdad() {
 		return edad;
 	}
 	
-	public void setEdad(int edad) {
-		this.edad = edad;
-	}
-	
+		
 	public Date getFecNac() {
 		return fecNac;
 	}
 	
-	public void setFecNac(Date fecNac) {
-		this.fecNac = fecNac;
+	public void setV(char v){
+		this.v=v;
 	}
-		
+	
+	public char getV(){
+		return v;
+	}
+	
 }
