31f10b476ad8177491214b83380aaba7d99ca362
arlzurg <alejandro.sonnor@gmail.com>
2016-06-14 00:59:54 -0500

    Persona

--- src/model/Persona.java
+++ src/model/Persona.java
@@ -1,5 +1,50 @@
 package model;
 
-public class Persona implements ICrud<T> {
+import java.util.Date;
 
+public class Persona{
+	private String dni;
+	private String nombres;
+	private String apellidos;
+	private String sexo;
+	private int edad;
+	private Date fecNac;
+	
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getNombres() {
+		return nombres;
+	}
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public Date getFecNac() {
+		return fecNac;
+	}
+	public void setFecNac(Date fecNac) {
+		this.fecNac = fecNac;
+	}
+		
 }
